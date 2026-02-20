145ac2f27b4cae7db09f4217d1df36edd9810138
Pablo Farias <pablofarias@MacBook-Air-de-Pablo.local>
2026-02-20 15:20:48 -0300

    Organizacao do codigo

--- attention.py
+++ attention.py
@@ -1,39 +1,17 @@
 import numpy as np
+
 def softmax(x):
     exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
     return exp_x / np.sum(exp_x, axis=1, keepdims=True)
 
-Q = np.array([[1, 0],
-              [0, 1]])
+def scaled_dot_product_attention(Q, K, V):
+    dk = Q.shape[1]
 
-K = np.array([[1, 0],
-              [0, 1]])
+    scores = Q @ K.T
+    scaled_scores = scores / np.sqrt(dk)
 
-print("Q:")
-print(Q)
+    attention_weights = softmax(scaled_scores)
 
-print("K:")
-print(K)
-V = np.array([[1, 2],
-              [3, 4]])
+    output = attention_weights @ V
 
-print("K transposta:")
-print(K.T)
-
-scores = Q @ K.T
-print("Q x K^T:")
-print(scores)
-dk = Q.shape[1]
-
-scaled_scores = scores / np.sqrt(dk)
-
-print("Scaled Scores:")
-print(scaled_scores)
-attention_weights = softmax(scaled_scores)
-
-print("Attention Weights:")
-print(attention_weights)
-output = attention_weights @ V
-
-print("Output Final:")
-print(output)
+    return output
